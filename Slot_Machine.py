e7f329ff674b9024ef2d531eb9955e3bfb4e150e
Luka khurtsidze <khurtsidzeluka0@gmail.com>
2025-08-06 00:40:46 +0400

    secon coommit

--- Slot_Machine.py
+++ Slot_Machine.py
@@ -2,6 +2,45 @@ MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [ [], [], [] ]
+
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(columns[0]):
+        for i, column in enumerate(columns):
+            print(column[row])
+            if i != len(columns) - 1:
+                print(column[row], '|')
+            else:
+                print(column[row])
+
 
 def deposit():
     while True:
